d655e74f4d733b9746ce390c10c5a25a50becba8
Kulsum Hameera <2021wa86222@wilp.bits-pilani.ac.in>
2025-07-14 17:57:49 +0530

    trigger CI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Devops Maven Project");
+        System.out.println("Hello Devops Maven Project Sample");
     }
 }
